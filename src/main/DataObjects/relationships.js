ae89b851cd694e341d405e15816388dd891ed1fd
J Bender <jayjayb100@msn.com>
2021-02-10 10:00:59 -0600

    reorganized and cleaned up template

--- src/main/DataObjects/relationships.js
+++
@@ -1,6 +0,0 @@
-const Enum = require('enum')
-const relationships = new Enum({
-    'Resident': 'RESIDENT',
-    'Friends': 'FRIENDS',
-    'Family':'FAMILY'
-});
--- src/test/index.spec.js
+++
@@ -1,3 +0,0 @@
-test('index test', ()=>{
-   expect(1).toBe(1);
-});
